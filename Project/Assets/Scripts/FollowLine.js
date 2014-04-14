25f25ccf1212845e170f124dfa539f74aa770cb2
Ryan Watkins <rwatk001@ucr.edu>
2014-04-14 14:38:54 -0700

    Line Drawing

---
+++ Project/Assets/Scripts/FollowLine.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if (LineMouse.myPoints != null) {
+		for(var point : Vector3 in LineMouse.myPoints) {
+			MoveObject(GameObject.Find("Player").transform, GameObject.Find("Player").transform.position, point, 0.5);
+		}
+	}
+}
+
+function MoveObject (thisTransform : Transform, startPos : Vector3, endPos : Vector3, time : float) {
+    var i = 0.0;
+    var rate = 1.0/time;
+    while (i < 1.0) {
+        i += Time.deltaTime * rate;
+        thisTransform.LookAt(endPos);
+        thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+		//Chaaracter Controller Beginnings
+//		var moveVector : Vector3 = endPos - startPos;
+//    	GameObject.Find("Player").GetComponent(CharacterController).Move(moveVector);
+        yield; 
+    }
+}
+
+function OnTriggerEnter (other : Collider) {
+	var playerPos : Vector3 = GameObject.Find("Player").transform.position;
+	var boundPos : Vector3 = new Vector3(playerPos.x,1,playerPos.z);
+	MoveObject(GameObject.Find("Player").transform, GameObject.Find("Player").transform.position, boundPos, 0.5);
+}
---
+++ Project/Assets/Scripts/SpawnBarrels.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function Start () {
+	for (var i : int = 0;i < 10; i++) {
+		var position: Vector3 = Vector3(Random.Range(-7.0, 7.0), i+2, Random.Range(-4.0, 4.0));
+		Instantiate(GameObject.Find("Barrel").transform, position, Quaternion.identity);
+	}
+}
